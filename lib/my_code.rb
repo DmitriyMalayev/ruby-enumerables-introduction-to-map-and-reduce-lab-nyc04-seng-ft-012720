115234d870700e810d970806981d0e670ff7974d
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2020-01-20 05:23:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,72 +7,72 @@ def map_to_negativize(source_array)
 i = 0 
 array = []
 
-while i < source_array.length do 
-  array << source_array[i] * -1
-  i += 1 
+# while i < source_array.length do 
+#   array << source_array[i] * -1
+#   i += 1 
   
-  #similar to array.push[i]
-end 
-return array 
-end 
+#   #similar to array.push[i]
+# end 
+# return array 
+# end 
 
 
-def map_to_no_change(source_array)   
-return source_array
-end 
+# def map_to_no_change(source_array)   
+# return source_array
+# end 
 
 
-def map_to_double(source_array)   
-i = 0 
-array = []
+# def map_to_double(source_array)   
+# i = 0 
+# array = []
 
-while i < source_array.length do 
-  array << source_array[i].to_i * 2
-  i += 1 
-end 
-return array 
-end 
+# while i < source_array.length do 
+#   array << source_array[i].to_i * 2
+#   i += 1 
+# end 
+# return array 
+# end 
 
-def map_to_square(source_array)   
-i = 0 
-array = []
+# def map_to_square(source_array)   
+# i = 0 
+# array = []
 
-while i < source_array.length do 
-  array << source_array[i].to_i ** 2
-  i += 1 
-end 
-return array 
-end 
+# while i < source_array.length do 
+#   array << source_array[i].to_i ** 2
+#   i += 1 
+# end 
+# return array 
+# end 
 
 
 
-def reduce_to_total(source_array, starting_point = 0)   
-    total = starting_point 
+# def reduce_to_total(source_array, starting_point = 0)   
+#     total = starting_point 
     
-      i = 0 
-      while i < source_array.length do 
-        total += source_array[i]
-        i += 1
-      end
-    total
-  end 
+#       i = 0 
+#       while i < source_array.length do 
+#         total += source_array[i]
+#         i += 1
+#       end
+#     total
+#   end 
 
 
 
-def reduce_to_all_true(source_array) 
-      i = 0 
-      while i < source_array.length do 
-        if source_array[i] == true 
-          return true 
-        if source_array[i] == false 
-          return false 
-    i += 1 
-  end
-  total
-end 
-# reduce_to_total([1,2,3,4])
+# def reduce_to_all_true(source_array) 
+#       i = 0 
+#       while i < source_array.length do 
+#         if source_array[i] == true 
+#           return true 
+#         if source_array[i] == false 
+#           return false 
+#     i += 1 
+#   end
+#   total
+# end 
+# # reduce_to_total([1,2,3,4])
 
-# reduce_to_total([1,2,2,4], 3)
+# # reduce_to_total([1,2,2,4], 3)
 
 
 
