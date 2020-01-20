0f05e042ab70239ea4c8d435e91c4d756737d072
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2020-01-20 06:15:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -18,32 +18,32 @@ return array
 end 
 
 
-# def map_to_no_change(source_array)   
-# return source_array
-# end 
+def map_to_no_change(source_array)   
+return source_array
+end 
 
 
-# def map_to_double(source_array)   
-# i = 0 
-# array = []
+def map_to_double(source_array)   
+i = 0 
+array = []
 
-# while i < source_array.length do 
-#   array << source_array[i].to_i * 2
-#   i += 1 
-# end 
-# return array 
-# end 
+while i < source_array.length do 
+  array << source_array[i].to_i * 2
+  i += 1 
+end 
+return array 
+end 
 
-# def map_to_square(source_array)   
-# i = 0 
-# array = []
+def map_to_square(source_array)   
+i = 0 
+array = []
 
-# while i < source_array.length do 
-#   array << source_array[i].to_i ** 2
-#   i += 1 
-# end 
-# return array 
-# end 
+while i < source_array.length do 
+  array << source_array[i].to_i ** 2
+  i += 1 
+end 
+return array 
+end 
 
 
 
@@ -64,9 +64,9 @@ def reduce_to_all_true(source_array)
       i = 0 
       while i < source_array.length do 
         return false if source_array[i] == false 
-    i += 1 
+        i += 1 
   end
-  total
+  return true
 end 
 
 
